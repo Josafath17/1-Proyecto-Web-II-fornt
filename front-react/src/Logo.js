ec85b79b650575edf5f5bd11f96c6549ef65f711
Josafath17 <105898971+Josafath17@users.noreply.github.com>
2024-03-06 16:17:55 -0600

    Login creation, but I need to give it a good style
    Tengo que corregir el estilo para que quede bien

---
+++ front-react/src/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="login-logo">
+      {/* You can use an image tag or any image loading library here */}
+      <img src="path/to/netflix-logo.png" alt="Netflix" />
+    </div>
+  );
+};
+
+export default Logo;
